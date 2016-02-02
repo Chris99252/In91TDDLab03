f14138d70fc60b7c61376e467b26cc2a64f54317
chris <s86610000@gmail.com>
2016-02-02 23:49:31 +0800

    commit Lab03 - V2 - AuthenticationSerce - IoC - public - setter：依賴注入，Stub（手刻）並使用公開屬性

---
+++ RsaSecureTokenTests/AuthenticationServiceTests - public - setter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RsaSecureToken;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace RsaSecureToken.Tests
+{
+    [TestClass()]
+    public class AuthenticationServiceTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            var target = new AuthenticationService();
+            target.profile = new StubProfileDao();
+            target.rsaToken = new StubRsaTokenDao();
+            var actual = target.IsValid("joey", "91000000");
+            //always failed
+            Assert.IsTrue(actual);
+        }
+    }
+
+    internal class StubProfileDao : IProfileDao
+    {
+        public string GetPassword(string account)
+        {
+            return "91";
+        }
+    }
+    internal class StubRsaTokenDao : IRsaToken
+    {
+        public string GetRandom(string account)
+        {
+            return "000000";
+        }
+    }
+}
