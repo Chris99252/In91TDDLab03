10a3e2ec0aaa601cb8b11883b8a0bd2ac7d80dc0
chris <s86610000@gmail.com>
2016-02-02 23:51:01 +0800

    commit Lab03 - V2-AuthenticationService - IoC - protected - virtual - function：依賴注入，Stub（手刻）並使用可覆寫的保護方法。

---
+++ RsaSecureTokenTests/AuthenticationServiceTests - protected - virtual - function.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RsaSecureToken;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace RsaSecureToken.Tests
+{
+    [TestClass()]
+    public class AuthenticationServiceTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            var target = new StubAuthenticationService();
+            var actual = target.IsValid("joey", "91000000");
+
+            //always failed
+            Assert.IsTrue(actual);
+        }
+    }
+
+    public class StubAuthenticationService : AuthenticationService
+    {
+        protected override ProfileDao GetProfileDao()
+        {
+            return new StubProfileDao();
+        }
+        protected override RsaTokenDao GetRsaToken()
+        {
+            return new StubRsaTokenDao();
+        }
+    }
+
+    public class StubProfileDao : AuthenticationService.ProfileDao
+    {
+        public override string GetPassword(string account)
+        {
+            return "91";
+        }
+    }
+    public class StubRsaTokenDao : AuthenticationService.RsaTokenDao
+    {
+        public override string GetRandom(string account)
+        {
+            return "000000";
+        }
+    }
+}
